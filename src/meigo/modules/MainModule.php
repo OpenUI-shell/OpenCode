e562ec9050ef7c925ebda3768261c6b855d7ff84
Meigo <73817505+meigoc@users.noreply.github.com>
2024-03-26 17:33:01 +0300

    Добавлена поддержка картинок в табах + сохранение файла функцией!

--- src/meigo/modules/MainModule.php
+++ src/meigo/modules/MainModule.php
@@ -1,6 +1,7 @@
 <?php
 namespace meigo\modules;
 
+use php\io\IOException;
 use std, gui, framework, app;
 
 
@@ -20,6 +21,37 @@ class MainModule extends AbstractModule
         foreach ($s as $str){
             $this->form('MainForm')->putVal($str . "/n");
         }
+    }
+    
+    function SaveFile() // Сохранение файла
+    {
+        // Инициализация
+        $a = $this->dirChooser->execute(); // Получаем директорию
+        $b = app()->form("MainForm")->tabs->tabs[app()->form("MainForm")->tabs->selectedIndex]->text; // Получаем имя файла сгенерированное ранее
+        // ============================
+        if (fs::isFile($a."\\".$b)) { // Проверяем существует ли файл, если существует, то текст будет заменяться, если нет - создаваться файл с текстом.
+            // существует
+            try {
+               Stream::putContents($a."\\".$b, app()->form("MainForm")->getActiveBrowser()->executeScript("editor.getValue();"));
+            } catch (IOException $e) {
+               alert('Ошибка записи: ' . $e->getMessage());
+            }
+        } else {
+            // не существует
+            fs::makeFile($a."\\".$b);
+            try {
+               Stream::putContents($a."\\".$b, app()->form("MainForm")->getActiveBrowser()->executeScript("editor.getValue();"));
+            } catch (IOException $e) {
+               alert('Ошибка записи: ' . $e->getMessage());
+            }
+        }
+        
+    }
+    
+    function addTabImage($image = 'res://.data/img/plus16.png')
+    {
+        $b = app()->form("MainForm")->tabs->tabs[app()->form("MainForm")->tabs->selectedIndex]->graphic = new UXImageView(new UXImage($image));
+        return $b;
     }
     
     
